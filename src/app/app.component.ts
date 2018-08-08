46076c120f23dc08143f258adf3d3a1810e97654
Jochen <jo@hassfurter.com>
2018-08-08 13:59:26 +0200

    No need to use jquery in Angular, here only as example to include non-ES6-conform JavaScript in an Angular-Project.
    a.)
    install jquery: ' npm i jquery --save'
    
    b.)
    insert typings into Angular-Project: 'npm i @types/jquery --save-dev' (=> tsconfig.json: 'typeRoots = node_modules/@types'; tsconfig.app.json: 'types = jquery')
    Only if modules are not ES6-conform and don't have type definition files '*.d.ts' - like header-definitions in C !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'tUp-root',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ThumbsUp';
+
+  constructor() {
+    $('body').css('backgroundColor', 'red');
+  }
 }
