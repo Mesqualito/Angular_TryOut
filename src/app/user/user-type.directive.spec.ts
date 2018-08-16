fcf1d20dc5e7c3da82b8cb8d61570393905a7872
Jochen <jo@hassfurter.com>
2018-08-16 13:55:54 +0200

    some more flags for creating directives with the Angular CLI (ng): 'ng g d user/userType --export --flat false -m app --prefix joGe --skip-import --spec false --dry-run' (where ''-m app' and '--skip-import' don't makes sense?!?)

--- src/app/user/user-type.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserTypeDirective } from './user-type.directive';
-
-describe('UserTypeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UserTypeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
