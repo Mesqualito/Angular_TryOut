dd2facf27a381d73ddabddbf9661189ed0b2d3c8
Jochen <jo@hassfurter.com>
2018-08-27 16:25:09 +0200

    ...with backticks (`) you can format the template-section within the typescript-file!

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -2,7 +2,12 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'in-user-list',
-  template: '<h1>User List</h1>'
+  template: `<h1>User List</h1>
+  <ul>
+    <li>User 1</li>
+    <li>User 2</li>
+    <li>User 3</li>
+  </ul>`
 })
 export class UserListComponent {
 
