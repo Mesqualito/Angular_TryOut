c3cf9eb3e29e6fd3cd3fe65c126ff08da02501c4
Jochen <jo@hassfurter.com>
2018-08-27 16:27:51 +0200

    ...but it's better to outsource the HTML-code into its own file!

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -2,12 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'in-user-list',
-  template: `<h1>User List</h1>
-  <ul>
-    <li>User 1</li>
-    <li>User 2</li>
-    <li>User 3</li>
-  </ul>`
+  templateUrl: './user-list.component.html'
 })
 export class UserListComponent {
 
