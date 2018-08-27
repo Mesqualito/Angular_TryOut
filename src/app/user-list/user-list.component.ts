990e6fb90927b98be5279963173820372dab9c05
Jochen <jo@hassfurter.com>
2018-08-27 16:19:09 +0200

    Angular: development driven by components („komponentenbasierte Entwicklung“) means, that components can have other components in their templates. So the whole architecture builds a tree.
    The base of a component is the component class (a .ts-file with „export class ComponentName“). The class gets meta-informations with a component-decorator:
       @Component({/*meta*/})
       export class ComponentName
    
    „@Component()“ is a method as you can see with its round brackets and has to be put direct before the declaration of the class.
    
    The component-decorator needs the following meta-informations:
    - „selector“ - „HTML-Knotenname“
    - every component has a template-level („Darstellungsschicht“) which will be recognized with
    	a.) „templateUrl“ (with seperate HTML-file)
    	b.) „template“ (HTML-elements directly in the typescript-file)
    - „styles“ or „styleUrls“, an optional list of style-Definitions

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'in-user-list',
+  template: ''
+})
+export class UserListComponent {
+
+  constructor() {
+    console.log('Hello UserList');
+  }
+}
