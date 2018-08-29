ed570d34b8adc951d4d9e143d95fc45932ab5b04
Jochen <jo@hassfurter.com>
2018-08-29 12:28:10 +0200

    Angular: Komponenten-Lebenszyklus
    1. Phase: constructor
    Jede Angular-Klasse verfügt über einen constructor, der beim Erzeugen einer neuen Instanz als erstes getriggert wird.
    
    2. Phase: ngOnChanges
    Nach der Erzeugung der Instanz, wird getriggert, wenn eine benutzerdefinierte Eigenschaft der Komponente eine Wertänderung erfährt.
    Bsp.: <userList [data]=“userList“> die ‘userList‘ hat die benutzerdefinierte Eigenschaft ‘data‘. ‘data‘ in eckigen Klammern bedeutet, dass der Wert sich ändern kann, abhängig von dem Wert ‘userList‘, der gebunden ist: ändert sich ‘userList‘, wird sich auch innerhalb der Komponente selbst der Wert ‘data‘ aktualisieren.
    Bei jeder Aktualisierung kann innerhalb der Komponentenklasse über die Methode ‘ngOnChanges‘ eine Information empfangen werden, was sich geändert hat. Sie wird initial gefeuert unmittelbar nach dem constructor und dann kontinuierlich, wenn sich Eigenschaften einer Komponente ändern.
    
    3. Phase: ngOnInit
    Die Methode ‘ngOnInit‘ kann einfach in die Komponentenklasse eingebaut werden. Wichtig ist, dass diese Funktion nur einmal gefeuert wird, sobald die Komponente fertig aufgebaut und ggf. alle Werte von außen empfangen hat und mit der Gesamtinitialisierung startet.
    
    4. Phase: ngDoCheck
    Angular überprüft, ob sich bestimmte Werte abhängig von Ereignissen geändert haben, damit darauf basierend ein Re-Rendering in der Vorlagenschicht ausgelöst wird. ‘ngDoCheck‘ wird immer dann getriggert, wenn Angular innerhalb der Komponente diese Änderungserkennung aktiviert hat.
    
    5. Phase: ngOnDestroy
    sagt aus, dass die Komponente die Bühne verlassen und Angular gerade alle Abhängigkeiten zerstört hat.
    
    Kinds-Komponenten- oder Content-Phasen – Checks aus den Elternkomponenten möglich!
    ‘Content‘, wenn die Inhalte innerhalb eines Templates transcodiert werden:
    
    ngAfterContentInit
    	<userList>
    		<user></user>
    		<user></user>
    	</userList>
    Die Komponente <user> ist hier kein „klassisches Kind“, das in der Vorlage liegt, sondern eine Kindskomponente, die als Inhalt transcodiert wurde, als Content für die Komponente. Sobald dieser Content sich zu Ende initialisiert hat, d.h. das Transcodieren abgeschlossen ist, wird ‘ngAfterContentInit‘ gefeuert.
    
    ngAfterContentChecked
    ‘ngAfterContentChecked‘ wird gefeuert, sobald die ‘ngDoCheck‘-Phase des Contents beendet wurde.
    
    Analog dazu kann auch geprüft werden, ob sich die Kinder in den Vorlagen geändert haben.
    D.h. ‘View‘, wenn innerhalb der Vorlagenschicht direkt Kindskomponenten existieren:
    
    ngAfterViewInit
    z.B. <user></user> im Template zu ‘UserList‘; hier kann auch ein anderer Lifecycle-Hook abgefangen werden. Hier, nachdem die Kindskomponente fertig initialisiert wurde.
    
    ngAfterViewChecked
    Hier, nachdem die Phase ‘ngDoCheck‘ der jeweiligen Kindskomponente durchgeführt wurde.

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'in-user-list',
@@ -6,9 +6,13 @@ import {Component, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./user-list.component.scss'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
 
   constructor() {
     console.log('the constructor: 1st stage of life of a component, here:  \'UserList\'');
+  }
+
+  ngOnInit(): void {
+    console.log( '\'OnInit\': 3rd stage of life of a component, here: \'UserList\'' );
   }
 }
