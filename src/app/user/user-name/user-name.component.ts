f425431d07751f77ca8c27f81333e8d4c7c05110
Jochen <jo@hassfurter.com>
2018-09-06 11:20:00 +0200

    Binding of attributes / Bindung von (HTML-Element-) Eigenschaften

--- src/app/user/user-name/user-name.component.ts
+++ src/app/user/user-name/user-name.component.ts
@@ -14,6 +14,8 @@ export class UserNameComponent implements OnInit, DoCheck {
     this._privateDescription = value;
   }
 
+  htmlContent = `<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>`
+
   description = 'Dies ist ein Beschreibungs-String!';
   private _privateDescription = 'auch mit Gettern und Settern möglich...';
 
