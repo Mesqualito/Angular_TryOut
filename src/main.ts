b5b636cecb415efdd9e747476ff1bd0fed18dfa5
Jochen <jo@hassfurter.com>
2018-09-06 10:47:06 +0200

    What is an attribute (Was ist eine Eigenschaft) in Angular?

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,11 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+let div: HTMLDivElement = document.createElement( 'div' ) as HTMLDivElement;
+
+// 'innerHTML is an attribute (Eigenschaft) of an instance of a 'div'
+div.innerHTML = '<h1>My div</h1>';
+
+document.body.appendChild( div );
+
