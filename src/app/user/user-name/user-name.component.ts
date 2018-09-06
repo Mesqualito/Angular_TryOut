9cb889bb76afc3b0f518a32149ec74cc195ca1f0
Jochen <jo@hassfurter.com>
2018-09-06 11:37:41 +0200

    'src' as Attribut of an 'img'-instance, a HTML-element

--- src/app/user/user-name/user-name.component.ts
+++ src/app/user/user-name/user-name.component.ts
@@ -14,10 +14,20 @@ export class UserNameComponent implements OnInit, DoCheck {
     this._privateDescription = value;
   }
 
-  htmlContent = `<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>`
-
   description = 'Dies ist ein Beschreibungs-String!';
   private _privateDescription = 'auch mit Gettern und Settern möglich...';
+
+  htmlContent: string = `<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>`;
+
+  imgPath = '/assets/img/Beer-belly 5.jpg';
+  altLabel = 'Beer-belly!';
 
   constructor() { }
 
