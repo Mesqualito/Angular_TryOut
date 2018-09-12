4ba1186f168217ada305f178bba9fd27b25ac2f2
Jochen <jo@hassfurter.com>
2018-09-12 09:58:30 +0200

    At the moment a 'user-name.component' is a child of 'user-list.component'. The 'user-name.component' takes a value for the attribute 'name' through binding with the decorator '@Input' from outside - here: from 'user-list.component'. These values can be modified - here with 'chgName ()' which has the decorator @HostListener on 'click'.
    Additional, there is a decorator '@HostBinding ()' on the css-class 'user-name': if 'isAdminUser = false', the css-class will be bound to the host (the 'user-name.component'). Test it with Augury, change "Benutzer Eins" to "Jochen Gebsattel". ATTENTION: seems not to work like before Angular 6, internal value-changes (with the '@HostListener') do not trigger the '@Input'! '@Input' is obviously triggered only with external input.
    
    So the parent-component tells the child-component, which value will be initially in 'name'.
    
    With '@Output', an EventEmitter can be declared and used to send information to the parent, here with 'this.nameChanged.emit( this.name )'.

--- src/app/user/user-name/user-name.component.ts
+++ src/app/user/user-name/user-name.component.ts
@@ -1,60 +1,65 @@
-import {Component,
-  HostBinding,
-  HostListener,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, HostBinding, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
-@Component ( {
-  selector   : 'in-user-name',
+@Component({
+  selector: 'in-user-name',
   templateUrl: './user-name.component.html',
-  styleUrls  : [ './user-name.component.scss' ]
-} )
+  styleUrls: ['./user-name.component.scss']
+})
 export class UserNameComponent implements OnInit, OnChanges {
 
+  /*
+  * EventEmitter() comes from the Reactive-X-framework
+  * and is used to send information as an event-stream
+  * from a child-component to the parent-component
+  *
+  * here we send the name to the parent, so
+  * the EventEmitter should have <string> as
+  * generic type
+  */
+  @Output ()
+  nameChanged: EventEmitter<string> = new EventEmitter();
 
-  get name (): string {
+  get name(): string {
     return this._name;
   }
-  @Input ()
-  set name ( value: string ) {
+
+  @Input()
+  set name( value: string ) {
     if ( value.trim() !== '' ) {
       this._name = value;
+      this.isAdminUser = this._name === 'Jochen Gebsattel';
+      console.log( `'this._name': ${this._name}, 'this.isAdminUser': ${this.isAdminUser}` );
     }
   }
 
-  @Input ()
+  @Input()
   city: string;
 
   userNameStyleClass = 'user-name';
 
-  private _name                = 'Jochen Haßfurter';
+  private _name = 'Jochen Haßfurter';
 
-  @HostBinding ( 'class.user-name' )
+  @HostBinding('class.user-name')
   isAdminUser = false;
 
-  constructor () {
+  constructor() {
   }
 
-  ngOnInit () {
+  ngOnInit() {
   }
 
-  @HostListener ( 'click', [ '$event',
-    '_name'
-  ] )
-  chgName ( event: MouseEvent, name: string ) {
-    console.log ( event, name );
-    if ( this._name !== 'Jochen Gebsattel' ) {
-      this._name       = 'Jochen Gebsattel';
-      this.isAdminUser = false;
+  @HostListener('click', ['$event', '_name'])
+  chgName(event: MouseEvent, name: string) {
+    console.log(event, name);
+    if (this._name !== 'Jochen Haßfurter') {
+      this._name = 'Jochen Haßfurter';
     } else {
-      this._name       = 'Jochen Haßfurter';
-      this.isAdminUser = true;
+      this._name = 'Jochen Gebsattel';
     }
+    this.nameChanged.emit( this.name );
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log ( changes );
+    console.log(changes);
   }
 }
